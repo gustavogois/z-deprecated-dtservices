c825614a97d961376d189ca95de0ad5c34c7e2fb
Gustavo Gois <gustavo.gois@gmail.com>
2016-05-25 21:00:59 +0100

    Atualização do script PopulaDados com Imóveis

--- BD/Popula BD.sql
+++ BD/Popula BD.sql
@@ -12,12 +12,18 @@ insert into servico (id, nome, valor, solicitanteId) values (6, "Abertura de Por
 insert into servico (id, nome, valor, solicitanteId) values (7, "Abertura de Porta (Fechadura Normal)", 20, 2);
 insert into servico (id, nome, valor, solicitanteId) values (8, "Abertura de Porta (Fechadura Especial)", 40, 2);
 
+delete from imovel;
+insert into imovel (id, inquilino, crp, endereco, codigoPostal) values (9, "João da Silva", 
+	"Cascais 1ªCRP, número 7049, matriz predial 16920, fracção B", 
+    "RUA ONDINA PEREIRA LOTE 3 175 RC ESQ, Buzano, São Domingos de Rana, Cascais", "2785-343");
+insert into imovel (id, inquilino, crp, endereco, codigoPostal) values (10, "Maria Pereira", 
+	"Arroja 3aCRP, número 1020, matriz predial 8845, fracção C", 
+    "Praceta das Orquídeas, 3, 4e, Famões, Castro, Odivelas", "2531-831");
+insert into imovel (id, inquilino, crp, endereco, codigoPostal) values (11, "Marcos Castro", 
+	"Colinas 6aCRP, número 200, matriz predial 3457, fracção A", 
+    "Praça de Portugal, 4, 4D, Plata, Lurdes, Odivelas", "2698-221");
+
 
 delete from hibernate_sequence;
-insert into hibernate_sequence values (9);
-
-select * from solicitante;
-select * from entidadedefacturacao;
-select * from servico;
-
+insert into hibernate_sequence values (12);
 
