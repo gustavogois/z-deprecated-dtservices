a3756f835b8c1631538d682659d5ea114a6de36f
Gustavo Gois <gustavo.gois@gmail.com>
2016-05-25 10:49:06 +0100

    1) Ajuste no modelo de dados (varchar 30 -> 60) 2) Atualização do script populadados, com a inserção de serviços.

--- BD/Popula BD.sql
+++ BD/Popula BD.sql
@@ -6,7 +6,18 @@ delete from entidadedefacturacao;
 insert into entidadedefacturacao (id, nome) values (3, "Caixa Econômica Montepio Geral");
 insert into entidadedefacturacao (id, nome) values (4, "Outra Entidade de Facturação ");
 
+delete from servico;
+insert into servico (id, nome, valor, solicitanteId) values (5, "Abertura de Porta (Fechadura Normal)", 10, 1);
+insert into servico (id, nome, valor, solicitanteId) values (6, "Abertura de Porta (Fechadura Especial)", 20, 1);
+insert into servico (id, nome, valor, solicitanteId) values (7, "Abertura de Porta (Fechadura Normal)", 20, 2);
+insert into servico (id, nome, valor, solicitanteId) values (8, "Abertura de Porta (Fechadura Especial)", 40, 2);
+
+
 delete from hibernate_sequence;
-insert into hibernate_sequence values (5);
+insert into hibernate_sequence values (9);
+
+select * from solicitante;
+select * from entidadedefacturacao;
+select * from servico;
 
 
