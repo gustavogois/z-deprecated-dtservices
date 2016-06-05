0ebba2038d3debb63ec437d471759ab916c39b37
Gustavo Gois <gustavo.gois@gmail.com>
2016-06-05 22:32:38 +0100

    Scripts de massa de dados atualizados de acordo com o novo modelo BD

--- BD/Popula BD - Massa de Dados.sql
+++ BD/Popula BD - Massa de Dados.sql
@@ -1,36 +1,33 @@
 -- tiposervico (5, "Abertura de Porta (Fechadura Normal)", 10, "");
 -- solicitante (1,"Banco Whitestar", "288653880", "243699012");
-insert into tiposervico_solicitante (id, tipoServicoId, solicitanteId, valor) values (9, 5, 1, 12);
+insert into tiposervico_solicitante (id, tipoServicoId, solicitanteId, valor) values (22, 5, 1, 12);
 
 -- tiposervico (5, "Abertura de Porta (Fechadura Normal)", 10, "");
 -- solicitante (2,"Banco de Negócios", "734221954", "217788432");
-insert into tiposervico_solicitante (id, tipoServicoId, solicitanteId, valor) values (10, 5, 2, 12);
+insert into tiposervico_solicitante (id, tipoServicoId, solicitanteId, valor) values (23, 5, 2, 12);
 
-insert into imovel (id, inquilino, crp, endereco, codigoPostal) values (11, "João da Silva", 
+insert into imovel (id, inquilino, crp, endereco, codigoPostal) values (24, "João da Silva", 
 	"Cascais 1ªCRP, número 7049, matriz predial 16920, fracção B", 
     "RUA ONDINA PEREIRA LOTE 3 175 RC ESQ, Buzano, São Domingos de Rana, Cascais", "2785-343");
 
+-- solicitante (1,"Banco Whitestar", "288653880", "243699012");
+insert into processocliente (id, codigo, descricao, dtFim, dtInicio, solicitanteId, estado_atual_Id)
+			values
+            (25, "PROC102030", "", null, null, 1, 16);
+            
 -- entidadedefacturacao (3, "Caixa Econômica Montepio Geral");
 -- solicitante (1,"Banco Whitestar", "288653880", "243699012");
 -- imovel (11, "João da Silva")
-insert into processo (id, entidadeFacturacaoId, solicitanteId, imovelId, comChaves, observacoes, dtSolicitacao, 
-                      dtCadastro, dtInicioExecucao, dtFinalizacao, dtFaturamento, dtRecebimento, estado)
-			values
-            (12, 3, 1, 11, true, "", null, null, null, null, null, null, 0);
+insert into processo(id, comChaves, estado, observacoes, entidadeFacturacaoId, processoClienteId, estado_atual_Id)
+	values
+    (26, true, 11, "", 3, 25, 11);
+
             
 -- solicitante (1,"Banco Whitestar", "288653880", "243699012");
 -- tiposervico (5, "Abertura de Porta (Fechadura Normal)", 10, "");
--- tiposervico_solicitante id = 9
--- processo 12            
-insert into servico (id, processoId, tipoServico_SolicitanteId, estado, dtCadastro, dtInicio, dtFim, valor)
+-- tiposervico_solicitante id = 22
+-- processo 26            
+insert into servico (id, dtCadastro, dtFim, dtInicio, observacoes, valor, processoId, tipoServico_SolicitanteId, estado_atual_Id)
 			values
-            (13, 12, 9, 0, null, null, null, 50);
+            (27, null, null, null, "", 50, 26, 22, 19);
             
-update hibernate_sequence set next_val=14;
-
--- insert into imovel (id, inquilino, crp, endereco, codigoPostal) values (10, "Maria Pereira", 
--- 	"Arroja 3aCRP, número 1020, matriz predial 8845, fracção C", 
---    "Praceta das Orquídeas, 3, 4e, Famões, Castro, Odivelas", "2531-831");
--- insert into imovel (id, inquilino, crp, endereco, codigoPostal) values (11, "Marcos Castro", 
--- 	"Colinas 6aCRP, número 200, matriz predial 3457, fracção A", 
---     "Praça de Portugal, 4, 4D, Plata, Lurdes, Odivelas", "2698-221");
--- BD/Popula BD.sql
+++ BD/Popula BD.sql
@@ -1,3 +1,5 @@
+update hibernate_sequence set next_val=1;
+
 insert into solicitante (id, nome, nif, telefone) values (1,"Banco Whitestar", "288653880", "243699012");
 insert into solicitante (id, nome, nif, telefone) values (2,"Banco de Negócios", "734221954", "217788432");
 
@@ -8,5 +10,22 @@ insert into tiposervico (id, nome, valor, descricao) values (5, "Abertura de Por
 insert into tiposervico (id, nome, valor, descricao) values (6, "Abertura de Porta (Fechadura Especial)", 20, "");
 insert into tiposervico (id, nome, valor, descricao) values (7, "Pintura", 5, "O preço está por metro quadrado.");
 
-update hibernate_sequence set next_val=8;
+insert into grupo_tipos_estado (id, nome) values (8, "Processo externo");
+insert into grupo_tipos_estado (id, nome) values (9, "Processo interno");
+insert into grupo_tipos_estado (id, nome) values (10, "Serviços");
+
+insert into tipos_de_estado (id, nome, grupo_estadoId) values (11, "Criado", 8);
+insert into tipos_de_estado (id, nome, grupo_estadoId) values (12, "Em execução", 8);
+insert into tipos_de_estado (id, nome, grupo_estadoId) values (13, "Finalizado", 8);
+insert into tipos_de_estado (id, nome, grupo_estadoId) values (14, "Faturado", 8);
+insert into tipos_de_estado (id, nome, grupo_estadoId) values (15, "Pago", 8);
+
+insert into tipos_de_estado (id, nome, grupo_estadoId) values (16, "Criado", 9);
+insert into tipos_de_estado (id, nome, grupo_estadoId) values (17, "Em execução", 9);
+insert into tipos_de_estado (id, nome, grupo_estadoId) values (18, "Finalizado", 9);
+
+insert into tipos_de_estado (id, nome, grupo_estadoId) values (19, "Criado", 10);
+insert into tipos_de_estado (id, nome, grupo_estadoId) values (20, "Em execução", 10);
+insert into tipos_de_estado (id, nome, grupo_estadoId) values (21, "Finalizado", 10);
+
 
