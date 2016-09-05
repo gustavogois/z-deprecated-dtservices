076ac04fddea377425220d99cdb922ebaf7e134f
Gustavo Gois <gustavo.gois@gmail.com>
2016-09-05 22:03:00 +0100

    delete cascade tiposervico

--- BD/Popula BD.sql
+++ BD/Popula BD.sql
@@ -19,16 +19,19 @@ insert into grupo_tipos_estado (id, nome) values (3, "Serviços");
 insert into tipos_de_estado (id, nome, grupo_estadoId) values (1, "Criado", 1);
 insert into tipos_de_estado (id, nome, grupo_estadoId) values (2, "Em execução", 1);
 insert into tipos_de_estado (id, nome, grupo_estadoId) values (3, "Finalizado", 1);
-insert into tipos_de_estado (id, nome, grupo_estadoId) values (4, "Faturado", 1);
-insert into tipos_de_estado (id, nome, grupo_estadoId) values (5, "Pago", 1);
+insert into tipos_de_estado (id, nome, grupo_estadoId) values (4, "Suspenso", 1);
 
-insert into tipos_de_estado (id, nome, grupo_estadoId) values (6, "Criado", 2);
-insert into tipos_de_estado (id, nome, grupo_estadoId) values (7, "Em execução", 2);
-insert into tipos_de_estado (id, nome, grupo_estadoId) values (8, "Finalizado", 2);
+insert into tipos_de_estado (id, nome, grupo_estadoId) values (5, "Criado", 2);
+insert into tipos_de_estado (id, nome, grupo_estadoId) values (6, "Em execução", 2);
+insert into tipos_de_estado (id, nome, grupo_estadoId) values (7, "Finalizado", 2);
+insert into tipos_de_estado (id, nome, grupo_estadoId) values (8, "Faturado", 2);
+insert into tipos_de_estado (id, nome, grupo_estadoId) values (9, "Pago", 2);
+insert into tipos_de_estado (id, nome, grupo_estadoId) values (10, "Suspenso", 2);
 
-insert into tipos_de_estado (id, nome, grupo_estadoId) values (9, "Criado", 3);
-insert into tipos_de_estado (id, nome, grupo_estadoId) values (10, "Em execução", 3);
-insert into tipos_de_estado (id, nome, grupo_estadoId) values (11, "Finalizado", 3);
+insert into tipos_de_estado (id, nome, grupo_estadoId) values (11, "Criado", 3);
+insert into tipos_de_estado (id, nome, grupo_estadoId) values (12, "Em execução", 3);
+insert into tipos_de_estado (id, nome, grupo_estadoId) values (13, "Finalizado", 3);
+insert into tipos_de_estado (id, nome, grupo_estadoId) values (14, "Suspenso", 3);
 
 INSERT INTO tipoDeUser( id, descricao ) VALUES( 0, 'Root' );
 INSERT INTO tipoDeUser( id, descricao ) VALUES( 1, 'Admin' );
