8bc354b340a2424bc7f2271e47e986dffc1fe8de
Gustavo Gois <gustavo.gois@gmail.com>
2017-01-01 16:10:30 +0000

    Imóveis - retirando tratamento e botões de latitude e longitude

--- dtServices-web/src/main/java/pt/gois/dtServices/controller/TipoServicoSolicitanteEditMB.java
+++ dtServices-web/src/main/java/pt/gois/dtServices/controller/TipoServicoSolicitanteEditMB.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
